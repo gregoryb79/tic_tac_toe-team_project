a16212da97095eec83e688ac51f5067bab5ea2a8
gregoryb79 <gregoryb79@gmail.com>
2025-02-02 20:08:02 +0200

    base

---
+++ dist/tic_tac_toe.js
@@ -0,0 +1,18 @@
+"use strict";
+exports.__esModule = true;
+exports.storeScore = exports.checkdiagonals = exports.checkrow = exports.checkcolumn = void 0;
+function checkcolumn() {
+    return winner;
+}
+exports.checkcolumn = checkcolumn;
+function checkrow() {
+    return winner;
+}
+exports.checkrow = checkrow;
+function checkdiagonals() {
+    return winner;
+}
+exports.checkdiagonals = checkdiagonals;
+function storeScore() {
+}
+exports.storeScore = storeScore;
---
+++ tic_tac_toe.ts
@@ -0,0 +1,17 @@
+export function checkcolumn(){
+    return winner;
+}
+
+export function checkrow(){
+    return winner;
+}
+
+export function checkdiagonals(){
+    return winner;
+}
+
+export function storeScore(){
+
+}
+
+export function
